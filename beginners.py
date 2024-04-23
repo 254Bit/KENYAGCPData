08b2940f78b99aa2d474aef276d5ee2d72e3e669
254Bit <oliviaayora@gmail.com>
2024-04-23 12:26:25 +0300

    First commit

---
+++ beginners.py
@@ -0,0 +1,27 @@
+import dash
+from dash import html #Used to create HTML Objects
+from dash import dcc
+import plotly.express as px # To create our graphs, a powerful functionality for processing data with minimal preparation
+import pandas as pd
+
+#Load and read the data
+data = pd.read_csv("gcp_economic_sectors.csv")
+#To create a plotly figure for use by dcc.graph()
+fig = px.line(data, x='Year', y='GCP', title='GCP over the years')
+
+
+app = dash.Dash(__name__) #Constructor: Is creating the dash application
+app.title = 'GCP over the years'
+
+app.layout = html.Div(
+    id='app-container',
+    children=[
+        html.H1('Kenya Gross County Product(2018-2020)Data Challenge'),
+        html.P('Year vs GCP'),
+        dcc.Graph(figure=fig)
+        
+    ]
+)
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
